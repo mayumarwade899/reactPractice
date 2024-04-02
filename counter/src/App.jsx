1f459b4fe49053f99e0d0b9ec60e68e0d3573bca
Mayur Marwade <mayumarwade899@gmail.com>
2024-04-02 09:38:01 +0530

    counter project added

---
+++ counter/src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  let [counter, setCounter] = useState(5)
+  // let counter = 5;
+
+  const addValue = ()=> {
+    // counter += 1;
+    setCounter(counter + 1);
+  }
+
+  const removeValue = ()=> {
+    setCounter(counter - 1);
+  }
+
+  return (
+    <>
+      <h1>Happy Coding</h1>
+      <h2>Counter value : {counter}</h2>
+
+      <button onClick = {addValue}
+      >Add value</button>
+      <br /><br />
+
+      <button onClick = {removeValue}
+      >Remove value</button>
+    </>
+  )
+}
+
+export default App
